c9590b25572fa8cca012fc757ded227beaff177d
--local <--local>
2026-05-23 19:58:18 +0300

    bot.py добавлена build_application(), main()

--- src/bot.py
+++ src/bot.py
@@ -9,6 +9,25 @@ handlers, callbacks, middlewares, конфигурация и инфрастру
 
 from core.config import get_api_key
 
-API_KEY = get_api_key()
+from telegram.ext import ApplicationBuilder
 
+# Создание приложения бота
+def build_application():
+    API_KEY = get_api_key()
+    application = ApplicationBuilder().token(API_KEY).build()
+
+    if application == None:
+        raise RuntimeError("Ошибка билда приложения")
+    
+    """
+        тут должно быть подключение хендлеров
+    """
+
+    return application
+
+# режим развертки - polling (poll/epoll)
+def main() -> None:
+    application = build_application() # Сам бот
+    
+    application.run_polling() # запускает боля в режиме ожидания, тут нужно поставить настройки (см библиотеку)
 

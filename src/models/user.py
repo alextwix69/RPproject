b10022f00deed2301f5a90721d51ba3bd609e003
--local <--local>
2026-05-24 23:37:41 +0300

    +db

--- src/models/user.py
+++ src/models/user.py
@@ -5,3 +5,24 @@ ORM-модель пользователя Telegram.
 timestamps и другая информация, которая нужна для сценариев профиля,
 регистрации и прав доступа.
 """
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String
+from datetime import date
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __name__ = "users"
+
+    id : Mapped[int] = mapped_column(
+        primary_key=True, 
+        unique=True, 
+        index=True
+    )
+    
+    username : Mapped[str] = mapped_column(String(255))
+    created_at : Mapped[date] = mapped_column(date)
+
+

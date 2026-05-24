b11060509ded1bf776ea201d2220d660cbb41eff
--local <--local>
2026-05-25 01:55:45 +0300

    +base in models.base

--- src/models/base.py
+++ src/models/base.py
@@ -5,3 +5,10 @@
 используемых полей, которые нужны моделям пользователей, платежей, товаров и
 других сущностей.
 """
+
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
+
+
